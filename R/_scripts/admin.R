cd0f0fc111b1983a4ea00c2d5942218d604a2842
Janko Thyson <janko.thyson@rappster.de>
2018-02-04 06:44:42 +0100

    Initial commit

---
+++ R/_scripts/admin.R
@@ -0,0 +1,45 @@
+
+
+# Settings ----------------------------------------------------------------
+
+library(usethis)
+repeat_initial <- FALSE
+
+# Misc --------------------------------------------------------------------
+
+use_mit_license("Janko Thyson")
+use_roxygen_md()
+use_readme_md()
+use_news_md()
+
+use_revdep()
+# Run checks with `revdepcheck::revdep_check(num_workers = 4)`
+
+# Data --------------------------------------------------------------------
+
+# x <- 1
+# y <- 2
+# use_data(x, y)
+# use_r()
+
+# GitHub credentials ------------------------------------------------------
+
+if (repeat_intial) {
+  cred <- git2r::cred_ssh_key(
+    normalizePath("~/../.ssh/id_rsa.pub", winslash = "/"),
+    normalizePath("~/../.ssh/id_rsa", winslash = "/")
+  )
+  repo <- git2r::repository(".git")
+  git2r::push(repo, credentials = cred)
+}
+
+# GitHub ------------------------------------------------------------------
+
+file.exists("~/../.ssh/id_rsa.pub")
+use_git()
+use_github(credentials = cred)
+
+# DevOps ------------------------------------------------------------------
+
+use_travis()
+use_coverage()
---
+++ README.md
@@ -0,0 +1,12 @@
+# cachefun
+
+The goal of cachefun is to ...
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+## basic example code
+```
+
