9a1f4213c0ff9fd944b4b38123e870f286847b2c
Janko Thyson <janko.thyson@rappster.de>
2018-02-04 06:58:43 +0100

    Uses GitHub - Moved my ssh key - ran 'usethis::use_github' - ran 'usethis::use_travis' - ran 'usethis::use_covr'

--- R/_scripts/admin.R
+++ R/_scripts/admin.R
@@ -1,5 +1,4 @@
 
-
 # Settings ----------------------------------------------------------------
 
 library(usethis)
@@ -25,9 +24,13 @@ use_revdep()
 # GitHub credentials ------------------------------------------------------
 
 if (repeat_intial) {
+  # cred <- git2r::cred_ssh_key(
+  #   normalizePath("~/../.ssh/id_rsa.pub", winslash = "/"),
+  #   normalizePath("~/../.ssh/id_rsa", winslash = "/")
+  # )
   cred <- git2r::cred_ssh_key(
-    normalizePath("~/../.ssh/id_rsa.pub", winslash = "/"),
-    normalizePath("~/../.ssh/id_rsa", winslash = "/")
+    "~/.ssh/id_rsa.pub",
+    "~/.ssh/id_rsa"
   )
   repo <- git2r::repository(".git")
   git2r::push(repo, credentials = cred)
@@ -35,11 +38,13 @@ if (repeat_intial) {
 
 # GitHub ------------------------------------------------------------------
 
-file.exists("~/../.ssh/id_rsa.pub")
-use_git()
-use_github(credentials = cred)
+# file.exists("~/../.ssh/id_rsa.pub")
+# use_git()
+# use_github(credentials = cred)
+use_github()
 
 # DevOps ------------------------------------------------------------------
 
 use_travis()
 use_coverage()
+# covr::package_coverage()
